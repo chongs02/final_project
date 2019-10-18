d2600b0ede991f5ba3247df28626b5e93e6b0543
chongs02 <chongs02@naver.com>
2019-10-18 11:24:17 +0900

    store, mupage, main tree change

--- MI-App/frontend/src/components/layouts/Main.js
+++ MI-App/frontend/src/components/layouts/Main.js
@@ -1,43 +1,38 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect, Route } from "react-router-dom";
 import { connect } from "react-redux";
 import { logout } from "../../actions/auth";
-import MovieInfo from "../MovieInfo"
+import MovieInfo from "../MovieInfo";
 
-class Nav extends Component {
+class Main extends Component {
   state = {
-    keyword : ""
+    keyword: ""
   };
 
-  handleChange = (e) =>{
-
+  handleChange = e => {
     this.setState({
-      keyword : e.target.value
-    })
-
-  }
+      keyword: e.target.value
+    });
+  };
 
   render() {
-    
     const { isAuthenticated, user } = this.props.auth;
-    const {keyword} = this.state
+    const { keyword } = this.state;
 
-    const mapToComponent = (data) => {
-      data = data.filter(
-        (title) => {
-          return title.movieNm.indexOf(keyword) > -1
-        }
-      )
-      return data.map((title, i) =>{
-        return(<MovieInfo title={title.movieNm} key={i}></MovieInfo>)
-      })
-    }
-
+    const mapToComponent = data => {
+      data = data.filter(title => {
+        return title.movieNm.indexOf(keyword) > -1;
+      });
+      return data.map((title, i) => {
+        return <MovieInfo title={title.movieNm} key={i}></MovieInfo>;
+      });
+    };
 
     const AuthLink = (
       <div>
         <span>{user ? `welcome ${user.username}` : ""}</span>
         <button onClick={this.props.logout}>Logout</button>
+        <Link to="/mypage">Mypage</Link>
       </div>
     );
 
@@ -50,10 +45,20 @@ class Nav extends Component {
 
     return (
       <nav className="nav">
-        <input className="search" type="text" placeholder="검색하시오" value={this.state.keword} onChange={this.handleChange}></input>
+        <input
+          className="search"
+          type="text"
+          placeholder="검색하시오"
+          value={this.state.keword}
+          onChange={this.handleChange}
+        ></input>
         <button>검색</button>
         <div>{isAuthenticated ? AuthLink : GuestLink}</div>
-        {this.props.isLoaded? mapToComponent(this.props.movieInfo):<div>Loading</div>}
+        {this.props.isLoaded ? (
+          mapToComponent(this.props.movieInfo)
+        ) : (
+          <div>Loading</div>
+        )}
       </nav>
     );
   }
@@ -71,4 +76,4 @@ const mapStateToProps = state => {
 export default connect(
   mapStateToProps,
   { logout }
-)(Nav);
+)(Main);
