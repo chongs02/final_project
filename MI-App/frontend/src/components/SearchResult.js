6dc00134567808fe504bca0c0f25989c656b63ff
blockhead4 <blockhead4@naver.com>
2019-10-20 20:01:04 +0900

    modified

--- MI-App/frontend/src/components/SearchResult.js
+++ MI-App/frontend/src/components/SearchResult.js
@@ -2,21 +2,22 @@ import React, { useState } from "react";
 import MovieInfo from "./MovieInfo";
 
 const SearchResult = ({ keyword, data }) => {
-  // const handleClick = key => {
-  //   console.log(key.id);
-  //   return key.id;
+  // handleClick = key => {
+  //   console.log(key.id, "is selected");
+  //   return key;
   // };
 
   data = data.filter(title => {
-    return title.movieNm.indexOf(keyword) > -1;
+    return title.movieNm.toLowerCase().indexOf(keyword) > -1;
   });
 
   return data.map((title, i) => {
+    // const id = title.id;
     return (
       <MovieInfo
         title={title}
         key={title.id}
-        // onClick={() => this.handleClick(title)}
+        // onClick={() => this.handleClick(id)}
       ></MovieInfo>
     );
   });
