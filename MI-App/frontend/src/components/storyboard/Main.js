9acbfa4a44ac2d829054d20294cec3aa013aafb8
chongs02 <chongs02@naver.com>
2019-10-21 15:19:43 +0900

    commit

--- MI-App/frontend/src/components/storyboard/Main.js
+++ MI-App/frontend/src/components/storyboard/Main.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link, Redirect, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { logout } from "../../actions/auth";
 import { movieInfo } from "../../actions/movieInfo";
