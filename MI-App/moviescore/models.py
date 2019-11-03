1fa0c91381cde2a9d922e8423ff63a7d24f5bbfb
chongs02 <chongs02@naver.com>
2019-11-04 04:22:20 +0900

    commit static

--- MI-App/moviescore/models.py
+++ MI-App/moviescore/models.py
@@ -7,14 +7,14 @@ class MovieScore(models.Model):
     id = models.AutoField(primary_key=True)
     movieCd = models.CharField(max_length=8)
     movieNm = models.CharField(max_length=128)
-    impression = models.PositiveSmallIntegerField()        # 감동
-    fear = models.PositiveSmallIntegerField()              # 공포
-    anger = models.PositiveSmallIntegerField()             # 분노
-    sadness = models.PositiveSmallIntegerField()           # 슬픔
-    fun = models.PositiveSmallIntegerField()               # 유쾌
-    boredom = models.PositiveSmallIntegerField()           # 지루함
-    positive = models.PositiveSmallIntegerField()          # 긍정
-    negative = models.PositiveSmallIntegerField()          # 부정
+    impression = models.PositiveSmallIntegerField(default=0)        # 감동
+    fear = models.PositiveSmallIntegerField(default=0)              # 공포
+    anger = models.PositiveSmallIntegerField(default=0)             # 분노
+    sadness = models.PositiveSmallIntegerField(default=0)           # 슬픔
+    fun = models.PositiveSmallIntegerField(default=0)               # 유쾌
+    boredom = models.PositiveSmallIntegerField(default=0)           # 지루함
+    positive = models.PositiveSmallIntegerField(default=0)          # 긍정
+    negative = models.PositiveSmallIntegerField(default=0)          # 부정
 
     # owner = models.ForeignKey(User, related_name="moviescore",
     #                           on_delete=models.CASCADE, null=True)
