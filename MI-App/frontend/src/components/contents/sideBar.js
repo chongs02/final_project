32d0fa6063f8d2c2067da594ac85999dede3635e
chongs02 <chongs02@naver.com>
2019-11-04 03:53:59 +0900

    add static visual

--- MI-App/frontend/src/components/contents/sideBar.js
+++ MI-App/frontend/src/components/contents/sideBar.js
@@ -17,27 +17,19 @@ export default class SideBar extends Component {
         }}
       >
         <div>
-          <NavLink
-            exact
-            to="/myPage/profile"
-            style={{ textDecoration: "none" }}
-          >
+          <NavLink exact to="/profile" style={{ textDecoration: "none" }}>
             <div>개인 정보</div>
           </NavLink>
         </div>
         <div>
-          <NavLink
-            exact
-            to="/myPage/mystatics"
-            style={{ textDecoration: "none" }}
-          >
+          <NavLink exact to="/mystatics" style={{ textDecoration: "none" }}>
             <div>나의 영화</div>
           </NavLink>
         </div>
         <div>
           <NavLink
             exact
-            to="/myPage/collaborative-seen"
+            to="/collaborative-seen"
             style={{ textDecoration: "none" }}
           >
             <div>봤어요 추천</div>
@@ -46,7 +38,7 @@ export default class SideBar extends Component {
         <div>
           <NavLink
             exact
-            to="/myPage/collaborative-like"
+            to="/collaborative-like"
             style={{ textDecoration: "none" }}
           >
             <div>좋아요 추천</div>
@@ -55,7 +47,7 @@ export default class SideBar extends Component {
         <div>
           <NavLink
             exact
-            to="/myPage/collaborative-hate"
+            to="/collaborative-hate"
             style={{ textDecoration: "none" }}
           >
             <div>걸러야할 영화</div>
---
+++ MI-App/frontend/src/components/contents/utils.js
@@ -0,0 +1,88 @@
+// object에서 특정한 key의 array 생성해줌
+export const pickInfo = (arr, instance) => {
+  let array = [];
+  arr.map(data => {
+    array.push(data[instance]);
+  });
+  return array;
+};
+
+//string => list => flatten
+export const stringToList = str => {
+  let arr = str.map(data =>
+    data
+      .replace(/['"]+/g, "")
+      .replace(/[\[\]']+/g, "", "")
+      .split(",")
+  );
+  arr = arr.flat();
+  arr = arr.map(data => data.trim());
+  console.log(arr);
+  return arr;
+};
+
+// array 속 가장 카운트가 많이된 value를 뽑아줌
+export const sorted = (type, arr) => {
+  let data = arr.reduce(function(acc, curr) {
+    if (typeof acc[curr] == "undefined") {
+      acc[curr] = 1;
+    } else {
+      acc[curr] += 1;
+    }
+
+    return acc;
+  }, {});
+  let sorted = Object.keys(data);
+  let newObject = {};
+  if (type === "descend") {
+    sorted
+      .sort(function(a, b) {
+        return data[b] - data[a];
+      })
+      .map(key => (newObject[key] = data[key]));
+  } else if (type === "ascend") {
+    sorted
+      .sort(function(a, b) {
+        return data[a] - data[b];
+      })
+      .map(key => (newObject[key] = data[key]));
+  }
+
+  return newObject;
+};
+
+// array 속 string 을 number로 바꿔줌
+export const arrayStringToNumber = arr => {
+  let numbers = arr.map(Number);
+  return numbers;
+};
+
+// 0값 제외하고 sum과 avg를 계산해줌
+export const calAverage = arr => {
+  const filtered = arr.filter(item => item !== 0);
+  let sum = 0;
+  let avg = 0;
+  if (filtered.length) {
+    sum = filtered.reduce((a, b) => a + b);
+    avg = sum / filtered.length;
+    return { avg, sum };
+  }
+};
+
+// array의 string의 앞 4자리를 빼줌
+export const getYear = arr => {
+  let data = arr.map(date => {
+    return date.slice(0, 4);
+  });
+  return data;
+};
+
+//array의 0제외하고 최소값을 찾아줌
+export const getMinNumber = arr => {
+  let data = arr.filter(item => item !== 0);
+  data = data.sort(function(a, b) {
+    return a - b;
+  });
+  let minValue = data.slice(0, 1);
+  return minValue[0];
+};
