3be8fe33b3498b34f63288f77a249de3e54c10c3
chongs02 <chongs02@naver.com>
2019-11-06 01:51:24 +0900

    commit

--- MI-App/users/serializer.py
+++ MI-App/users/serializer.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from rest_framework import serializers
-from .models import Profile, Person
+from .models import Profile
 
 
 
